119bd12a0ee9dbe137b71037ae135537ec4cbd45
koladeore <koladeore@gmail.com>
2019-09-10 07:56:26 +0100

    db-connection

---
+++ server/database/config/config.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DEV,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres',
+    logging: false,
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+};
---
+++ server/database/models/user.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    verified: DataTypes.BOOLEAN
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
